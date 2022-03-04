029b8808f798dd8d0a1546e5313a81cebfc5141b
Bradley Gathers <34373485+baxtrax@users.noreply.github.com>
2022-03-03 19:56:51 -0500

    Fixing subtraction failures

--- alu32.v
+++ alu32.v
@@ -1,46 +1,50 @@
-/* A 32bit ALU.
+/* A test bench to test the functionality of
+ * the 32bit ALU.
  *
  * Author: Bradley Gathers
  * Date: 3/3/2022
 */
-module alu32(a,b,opCode,out,S);
+module alu32_tb();
 
 	// I/O and Parameters
 	parameter N = 32;
+	reg [N-1:0] a,b;               
+	reg [4:0] opCode;
+	wire [N-1:0] out;
+	wire [3:0] S;
+	
+	integer i;
+	
+	// Initialize the DUT
+	alu32 DUT(.a(a),.b(b),.opCode(opCode),.out(out),.S(S));
 
-	input [N-1:0] a,b;                
-	input [4:0] opCode;
-	output [N-1:0] out; 
-	output [3:0] S;
-	 
-	reg [N-1:0] ALU_Result;
-	wire [N-2:0] tmpA = a[N-2:0];
-	wire [N-2:0] tmpB = b[N-2:0];
-	
-	//Statuses
-	wire co, neg, over, zero;
-	
-	// Assign Statements for Statuses
-	assign neg = a[N-1] ^ b[N-1]; // Negative
-	assign co = ALU_Result[N-1];  // Carryout
-	assign over = ((a[N-1] & b[N-1]) | (~a[N-1] & ~b[N-1])); // Overflow
-	assign zero = (ALU_Result[N-2:0] == 0) ? 1'b1 : 1'b0; // Zero
-	assign S = {co, neg, over, zero};
-	
-	// Output Assignment
-	assign out = {neg, ALU_Result[N-2:0]}; // ALU out
-	
-	always @(*) begin
-		case(opCode)
-			4'b0000: ALU_Result = tmpA + tmpB;    // Addition
-			4'b0001: ALU_Result = tmpA - tmpB;    // Subtraction
-			4'b0010: ALU_Result = tmpA ^ tmpB;    // Logical xor 
-			4'b0011: ALU_Result = tmpA & tmpB;    // Logical and 
-			4'b0100: ALU_Result = tmpA | tmpB;    // Logical or
-			4'b0101: ALU_Result = ~(tmpA | tmpB); // Logical nor
-			4'b0110: ALU_Result = tmpA<<tmpB;     // Logical shift left
-			4'b0111: ALU_Result = tmpA>>tmpB;     // Logical shift right
-			default: ALU_Result = tmpA + tmpB;    // Default is add 
-		endcase
+	// Initial Setup
+	initial begin
+		//Test Opcodes
+		a = 32'h7;
+		b = 32'h2;
+		opCode = 5'h0;
+		#10;
+		for (i=0; i<8; i=i+1) begin
+			opCode = opCode + 1'b1;
+			#10;
+		end
+		#20;
+		opCode = 5'h0;
+		
+		// Test co & overflow
+		a = 32'hffffffff;
+		b = 32'hffffffff;
+		#10;
+		// Test zero condition
+		a = 32'h00000000;
+		b = 32'h00000000;
+		#10;
+		//Test negative flag
+		a = 32'h80000004;
+		b = 32'h00000001;
+		#10;
+		
+		#100 $stop;
 	end
 endmodule
